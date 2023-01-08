26d685d385290be035e78701c1d704480f505e7a
Manh Tran <tienmanh.tran68@gmail.com>
2023-01-08 16:28:17 +0700

    add StudentService class

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -12,14 +12,14 @@ import java.util.List;
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents() {
-        return List.of(new Student(
-                1L,
-                "Maria",
-                "tienmanh.tran68@gmail.com",
-                LocalDate.of(2000, Month.JANUARY, 5),
-                21
-        ));
+        return studentService.getStudents();
     }
 }
